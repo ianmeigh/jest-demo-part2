4e701dbb06da4419368fc21d34b5459adc10f37d
ianmeigh <ianmeigh@googlemail.com>
2022-05-04 21:00:19 +0100

    Add game object tests and code

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -1,3 +1,35 @@
 /**
  * @jest-environment jsdom
  */
+
+const { game } = require("../game");
+
+beforeAll(() => {
+  let fs = require("fs");
+  const fileContents = fs.readFileSync("index.html", "utf-8");
+  document.documentElement.innerHTML = fileContents.toString();
+});
+
+describe("game object contains the correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("currentGame key contains Array", () => {
+    expect(Array.isArray(game.currentGame)).toBe(true);
+  });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices key contains Array", () => {
+    expect(Array.isArray(game.choices)).toBe(true);
+  });
+  test("choices contains the correct ids", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
+});
